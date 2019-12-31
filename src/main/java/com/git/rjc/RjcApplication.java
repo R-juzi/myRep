24413590654858ffc55e5f7b5fdbf6b2e1522366
R橘子 <2313397011@qq.com>
2019-12-31 12:23:42 +0800

    push project

---
+++ src/main/java/com/git/rjc/RjcApplication.java
@@ -0,0 +1,13 @@
+package com.git.rjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RjcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/git/rjc/RjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.rjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
