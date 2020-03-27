6c8fd07a927171217b389a7961214bb985899d58
R橘子 <2313397011@qq.com>
2020-03-27 20:52:01 +0800

    CanNotFoundAccountException

---
+++ src/main/java/com/git/rjc/exception/CanNotFoundAccountException.java
@@ -0,0 +1,10 @@
+package com.git.rjc.exception;
+
+/**
+ * @program: rjc
+ * @description: 找不到账号
+ * @author: R红茶
+ * @create: 2020-03-27 20:51
+ **/
+public class CanNotFoundAccountException extends ServiceException {
+}
